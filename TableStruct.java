4d6a90f94c3e6bcf067b27235096d58dd2c47a62
dk2k <dk2k@users.noreply.github.com>
2020-07-06 16:23:30 +0300

    Delete TableStruct.java

--- TableStruct.java
+++
@@ -1,8 +0,0 @@
-import java.util.HashMap;
-import java.util.Map;
- 
-public class TableStruct { 
-  String TableName; 
-  Map<String, TypeDesc> Variables = 
-              new HashMap<String, TypeDesc> ();
-}
